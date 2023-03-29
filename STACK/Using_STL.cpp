9f45e1960f97daa7b299cad06d0d2cc84ae01e19
Karthik <reddypolukarthikeya@gmail.com>
2023-03-29 21:36:50 +0530

    Comments added

--- STACK/Using_STL.cpp
+++ STACK/Using_STL.cpp
@@ -21,18 +21,25 @@ int main() {
             case 1:
                 cout << endl << "Enter the element to push into the stack: ";
                 cin >> temp;
+                //Using the push member funciton to insert the element into the stack.
                 s.push(temp);
             break;
             case 2:
+                // empty() memeber function returns true if the stack is empty and false if not empty.
                 if(s.empty())  {
                     cout << endl << "Stack Underflow..!!";
+                    // To avoid Segmentation Fault break statement is added.
                     break;
                 }
                 cout << endl << "The element popped out is: ";
+                //The top() member function returns the top most element of the stack.
                 cout << s.top();
+                //The pop() member function deletes the element from the stack.
                 s.pop();
             break;
             case 3:
+            // Since pop() function deletes the element from the stack which 
+            // brings the temporary stack to print the elements in the stack.
                 t = s;
                 while(!t.empty()) {
                     cout << t.top() << endl;
