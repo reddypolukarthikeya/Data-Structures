149f5a07f2dd9477377719545fceb08131051496
reddypolukarthikeya <reddypolukarthikeya@gmail.com>
2023-04-01 12:42:37 +0530

    added comments

---
+++ Linked List/Singly_Linked_List_Remove_Duplicates.cpp
@@ -0,0 +1,119 @@
+#include <iostream>
+
+// Using 'nampespace std;' is considered as bad programming practice.
+using std::cin;
+using std::cout;
+using std::endl;
+
+class Node {
+    public:
+        int data;
+        Node *next;
+    //Unparameterized constructor
+    Node() {
+        data = -1;
+        next = NULL;
+    }
+    //Parameterized constructor
+    Node (int data) {
+        this->data = data;
+        this->next = NULL;
+    }
+    //Desstructor to destroy the nodes and free memory allocated to the nodes.
+    ~Node() {
+        cout << endl << "All the node have been destroyed..!!";
+    }
+};
+
+  
+class LinkedList {
+    public:
+        Node *head = NULL;
+        //Destructor to detroy the linked list and free memory allocated the list.
+        ~LinkedList() {
+            cout << endl << "List has been Destroyed...!!!";
+        }
+        void Insert(int data);
+        void RemoveDuplicates();
+        void Display();
+};
+
+void LinkedList::Insert(int data) {
+    //If the list is empty create a Node
+    Node *newNode = new Node(data);
+    if (head == NULL) {
+        head = newNode;
+        return;
+    }
+    else {
+        Node *temp = head;
+        //Traverse to the end of the list
+        while(temp->next != NULL) {
+            temp = temp->next;
+        }
+        temp->next = newNode;
+        return;
+    }
+}
+
+void LinkedList::Display() {
+    //Check if the list is empty.
+    if (head == NULL) {
+        cout << "List is empty" << endl;
+        return;
+    }
+    //If the list is not empty
+    Node *temp = head;
+    while (temp != NULL) {
+        cout << temp->data << endl;
+        temp = temp->next;
+    }
+}
+
+void LinkedList::RemoveDuplicates() {
+    //Check if the list is empty.
+    if (head == NULL) 
+        return;
+    Node *temp = head;
+    //If the data of the current node is equal to the data of the next node then remove the next node.
+    while (temp->next != NULL) {
+        if (temp->data == temp->next->data) {
+            temp->next = temp->next->next;
+        }
+        else 
+            temp = temp->next;
+    }
+}
+
+int main() {
+
+    //Object of class LinkedList.
+    LinkedList list;
+    int temp,ch;
+    //Menu driven program.
+    do {
+        cout << "\t\t      MENU" << endl;
+        cout << "\t\t1.Insert" << endl;
+        cout << "\t\t2.Remove Duplicates" << endl;
+        cout << "\t\t3.Display" << endl;
+        cout << "\t\t4.Exit" << endl;
+        cout << "Enter your choice: [  ]\b\b";
+        cin >> ch;
+        switch(ch) {
+                case 1:
+                    cout << endl << "Enter the data to insert: ";
+                    cin >> temp;
+                    list.Insert(temp);
+                break;
+                case 2:
+                    list.RemoveDuplicates();
+                break;
+                case 3:
+                    list.Display();
+                break;
+                case 4:
+                    return 0;
+                break;
+        }
+    }while(ch != 4);
+}
