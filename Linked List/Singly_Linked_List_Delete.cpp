bd64c4d602f7b1db4e56f212a56a3b0f9dab05b9
reddypolukarthikeya <reddypolukarthikeya@gmail.com>
2023-04-01 10:46:34 +0530

    Singly Linked List operations: 4.Delete at TAIL

--- Linked List/Singly_Linked_List_Delete.cpp
+++ Linked List/Singly_Linked_List_Delete.cpp
@@ -34,6 +34,7 @@ class LinkedList {
             cout << endl << "List has been Destroyed...!!!";
         }
         void DeleteAtTail();
+        void Insert(int);
         void Display();
 };
 
@@ -61,6 +62,24 @@ void LinkedList::DeleteAtTail() {
     temp->next = NULL;
 }
 
+void LinkedList::Insert(int data) {
+    //If the list is empty create a Node
+    Node *newNode = new Node(data);
+    if (head == NULL) {
+        head = newNode;
+        return;
+    }
+    else {
+        Node *temp = head;
+        //Traverse to the end of the list
+        while(temp->next != NULL) {
+            temp = temp->next;
+        }
+        temp->next = newNode;
+        return;
+    }
+}
+
 void LinkedList::Display() {
     //Check if the list is empty.
     if (head == NULL) {
@@ -83,22 +102,27 @@ int main() {
     //Menu driven program.
     do {
         cout << "\t\t      MENU" << endl;
-        cout << "\t\t1.Delete at TAIL" << endl;
-        cout << "\t\t2.Display" << endl;
-        cout << "\t\t3.Exit" << endl;
-        cout << "Enter your choice: [  ]\b\b" << endl;
+        cout << "\t\t1.Insert" << endl;
+        cout << "\t\t2.Delete at TAIL" << endl;
+        cout << "\t\t3.Display" << endl;
+        cout << "\t\t4.Exit" << endl;
+        cout << "Enter your choice: [  ]\b\b";
         cin >> ch;
         switch(ch) {
                 case 1:
+                    cout << endl << "Enter the data to insert: ";
                     cin >> temp;
-                    list.DeleteAtTail();
+                    list.Insert(temp);
                 break;
                 case 2:
-                    list.Display();
+                    list.DeleteAtTail();
                 break;
                 case 3:
+                    list.Display();
+                break;
+                case 4:
                     return 0;
                 break;
         }
-    }while(ch != 3);
+    }while(ch != 4);
 }
