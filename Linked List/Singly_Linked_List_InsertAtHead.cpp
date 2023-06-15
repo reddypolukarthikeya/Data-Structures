1c7e63c7a35db46ebe3ebfd9216285147643a515
reddypolukarthikeya <reddypolukarthikeya@gmail.com>
2023-06-15 23:09:25 +0530

    Final commit

--- Linked List/Singly_Linked_List_InsertAtHead.cpp
+++ Linked List/Singly_Linked_List_InsertAtHead.cpp
@@ -1,14 +1,12 @@
 #include <iostream>
+#include <memory>
 
 // Using 'nampespace std;' is considered as bad programming practice.
-using std::cin;
-using std::cout;
-using std::endl;
 
 class Node {
     public:
         int data;
-        Node *next;
+        std::unique_ptr<Node> next;
     //Unparameterized constructor
     Node() {
         data = -1;
@@ -23,48 +21,57 @@ class Node {
 
   
 class LinkedList {
+    private:
+        std::unique_ptr<Node> head;
+        std::unique_ptr<Node> tail;
     public:
-        Node *head = nullptr;
-        //Destructor to detroy the linked list and free memory allocated the list.
-        // Default destructor only deletes head (allocated by constructor).
-        // We need destructor to free the memory used by all individual nodes.
-        ~LinkedList() {
-            Node *temp = head;
-            while(head != nullptr) {
-                head = head->next;
-                delete temp;
-                temp = head;
-            }
-            cout << endl << "List has been Destroyed...!!!";
+        // Unparameterized constructor
+        LinkedList() {
+            head = nullptr;
+            tail = nullptr;
         }
+        /*
+            // Destructor to detroy the linked list and free memory allocated the list.
+            // Default destructor only deletes head (allocated by constructor).
+            // We need destructor to free the memory used by all individual nodes.
+            ~LinkedList() {
+                Node *temp = head;
+                while(head != nullptr) {
+                    head = head->next;
+                    delete temp;
+                    temp = head;
+                }
+                std::cout << "\nList has been Destroyed...!!!";
+            }
+        */
         void InsertAtHead(int data);
         void Display();
 };
 
 void LinkedList::InsertAtHead(int data) {
-    Node *newNode = new Node(data);
+    std::unique_ptr<Node> newNode = std::make_unique<Node>(data);
     //if the list is empty 
     if (head == nullptr) {
-        head = newNode;
+        head = move(newNode);
         return;
     }
     //if the list is not empty
-    newNode->next = head;
-    head = newNode;
+    newNode->next = move(head);
+    head = move(newNode);
     return;
 }
 
 void LinkedList::Display() {
     //Check if the list is empty.
     if (head == nullptr) {
-        cout << "List is empty" << endl;
+        std::cout << "List is empty\n";
         return;
     }
     //If the list is not empty
-    Node *temp = head;
+    Node *temp = head.get();
     while (temp != nullptr) {
-        cout << temp->data << endl;
-        temp = temp->next;
+        std::cout << temp->data << " ";
+        temp = temp->next.get();
     }
 }
 
@@ -75,16 +82,16 @@ int main() {
     int temp,ch;
     //Menu driven program.
     do {
-        cout << "\t\t      MENU" << endl;
-        cout << "\t\t1.Insert at HEAD" << endl;
-        cout << "\t\t2.Display" << endl;
-        cout << "\t\t3.Exit" << endl;
-        cout << "Enter your choice: [  ]\b\b" << endl;
-        cin >> ch;
+        std::cout << "\t\t      MENU\n";
+        std::cout << "\t\t1.Insert at HEAD\n";
+        std::cout << "\t\t2.Display\n";
+        std::cout << "\t\t3.Exit\n";
+        std::cout << "Enter your choice: [  ]\b\b";
+        std::cin >> ch;
         switch(ch) {
                 case 1:
-                    cout << "Enter the element to insert: ";
-                    cin >> temp;
+                    std::cout << "Enter the element to insert: ";
+                    std::cin >> temp;
                     list.InsertAtHead(temp);
                 break;
                 case 2:
@@ -92,6 +99,9 @@ int main() {
                 break;
                 case 3:
                     return 0;
+                break;
+                default:
+                    std::cout << "\nInvalid choice..!!";
                 break;
         }
     }while(ch != 3);
