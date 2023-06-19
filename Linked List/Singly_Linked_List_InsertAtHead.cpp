5e05c581dedda698d71e50837d4e4edee8c39549
reddypolukarthikeya <reddypolukarthikeya@gmail.com>
2023-06-19 19:47:01 +0530

    Final commit

--- Linked List/Singly_Linked_List_InsertAtHead.cpp
+++ Linked List/Singly_Linked_List_InsertAtHead.cpp
@@ -1,18 +1,16 @@
 #include <iostream>
-#include <memory>
 
 // Using 'nampespace std;' is considered as bad programming practice.
-// Since "smart pointers" are used there is no need to use a destructor.
 
 class Node {
     public:
         int data;
-        std::unique_ptr<Node> next;
+        Node *next;
 
-    //Unparameterized constructor
+    // Unparameterized constructor
     Node() : data(-1), next(nullptr) {};
     
-    //Parameterized constructor
+    // Parameterized constructor
     explicit Node (int data) : data(data), next(nullptr) {};
 };
 
@@ -21,70 +19,62 @@ class LinkedList {
 
     private:
 
-        std::unique_ptr<Node> head;
-        std::unique_ptr<Node> tail;
+        Node *head;
 
     public:
 
         // Default constructor 
-        LinkedList() : head(nullptr), tail(nullptr) {};
-        // Although there is no need to use a destructor because of the smart pointers,
-        // it is still good to have a destructor to ensure proper deallocation.
+        LinkedList() : head(nullptr) {};
+        // Destructor to detroy the linked list and free memory allocated the list.
+        // Default destructor only deletes head (allocated by constructor).
+        // We need destructor to free the memory used by all individual nodes.
         ~LinkedList() {
-            std::cout << "\nList has been destroyed..!!";
-        }
-        /*
-            // Destructor to detroy the linked list and free memory allocated the list.
-            // Default destructor only deletes head (allocated by constructor).
-            // We need destructor to free the memory used by all individual nodes.
-            ~LinkedList() {
-                Node *temp = head;
-                while(head != nullptr) {
-                    head = head->next;
-                    delete temp;
-                    temp = head;
-                }
-                std::cout << "\nList has been Destroyed...!!!";
+            Node *temp = head;
+            while(head != nullptr) {
+                head = head->next;
+                delete temp;
+                temp = head;
             }
-        */
+            std::cout << "\nList has been Destroyed...!!!";
+        }
         void InsertAtHead(int data);
         void Display();
 };
 
 void LinkedList::InsertAtHead(int data) {
-    std::unique_ptr<Node> newNode = std::make_unique<Node>(data);
-    //if the list is empty 
+    Node *newNode = new Node(data);
+    // If the list is empty 
     if (head == nullptr) {
-        head = std::move(newNode);
+        head = newNode;
         return;
     }
-    //if the list is not empty
-    newNode->next = std::move(head);
-    head = std::move(newNode);
+    // If the list is not empty
+    newNode->next = head;
+    head = newNode;
 }
 
 void LinkedList::Display() {
-    //Check if the list is empty.
+    // Check if the list is empty.
     if (head == nullptr) {
         std::cout << "List is empty\n";
         return;
     }
-    //If the list is not empty
-    Node *temp = head.get();
+    // If the list is not empty
+    Node *temp = head;
     while (temp != nullptr) {
         std::cout << temp->data << " ";
-        temp = temp->next.get();
+        temp = temp->next;
     }
 }
 
 int main() {
 
     {
-        //Object of class LinkedList.
+        // Object of class LinkedList.
         LinkedList l;
         int ch;
         int temp;
-        //Menu driven program.
+        // Menu driven program.
         do {
             std::cout << "\t\t      MENU\n";
             std::cout << "\t\t1.Insert at HEAD\n";
