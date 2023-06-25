b1b14392cfbf2a51d9e37bce29cc98f5262f3a40
reddypolukarthikeya <reddypolukarthikeya@gmail.com>
2023-06-25 11:46:33 +0530

    Final commit

--- Queues/Queue_using_Array.cpp
+++ Queues/Queue_using_Array.cpp
@@ -7,6 +7,7 @@ class Queue {
     private:
         int top;
         int front;
+        int Arr[10];
     public:
         Queue() {
             top = -1;
@@ -14,4 +15,77 @@ class Queue {
         }
         void Enqueue(int data);
         int Dequeue();
+        void Display();
 };
+
+void Queue::Enqueue(int data) {
+    // Check if the queue is empty.
+    if (front == -1) {
+        Arr[++top] = data;
+        front++;
+        return;
+    }
+    if (top > 9) {
+        std::cout << "\nQueue is full.";
+        return;
+    }
+    Arr[++top] = data;
+}
+
+int Queue::Dequeue() {
+    // Check if the queue is empty.
+    if (front == -1) {
+        std::cout << "\nQueue is empty..!!";
+        return -1;
+    }
+    int temp = Arr[front];
+    front++;
+    return temp;
+}
+
+void Queue::Display() {
+    // Check if the queue is empty.
+    if (front == -1) {
+        std::cout << "\nQueue is empty..!!";
+        return;
+    }
+    while (front <= top)  {
+        std::cout << "\n" << Arr[front];
+        front++;
+    }
+}
+
+int main() {
+    Queue q;
+    int ch;
+    do {
+        std::cout << "\n\tMenu";
+        std::cout << "\n1.Enqueue";
+        std::cout << "\n2.Dequeue";
+        std::cout << "\n3.Display";
+        std::cout << "\n4.Exit";
+        std::cout << "\n\tEnter your choice : [ ]\b\b";
+        std::cin >> ch;
+        switch (ch) {
+            case 1:
+                int temp;
+                std::cout << "\nEnter the element to insert into the queue: ";
+                std::cin >> temp;
+                q.Enqueue(temp);
+            break;
+            case 2:
+                std::cout << "\nThe element deleted from the queue is : ";
+                std::cout << q.Dequeue();
+            break;
+            case 3:
+                q.Display();
+            break;
+            case 4:
+                return 0;
+            break;
+            default:
+                std::cout << "\nInvalid choice..!!";
+            break;
+        }    
+    }while(ch != 4);
+}
