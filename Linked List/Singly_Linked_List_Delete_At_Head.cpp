11174772d7fb1da46ee1231928c40cdc2da4c94f
reddypolukarthikeya <reddypolukarthikeya@gmail.com>
2023-07-07 23:10:06 +0530

    Final commit

--- Linked List/Singly_Linked_List_Delete_At_Head.cpp
+++ Linked List/Singly_Linked_List_Delete_At_Head.cpp
@@ -1,20 +1,17 @@
 #include <iostream>
 
 // Using 'nampespace std;' is considered as bad programming practice.
-using std::cin;
-using std::cout;
-using std::endl;
 
 class Node {
     public:
         int data;
         Node *next;
-    //Unparameterized constructor
+    // Unparameterized constructor
     Node() {
         data = -1;
         next = nullptr;
     }
-    //Parameterized constructor
+    // Parameterized constructor
     Node (int data) {
         this->data = data;
         this->next = nullptr;
@@ -23,9 +20,19 @@ class Node {
 
   
 class LinkedList {
+
+    private:
+
+        Node *head;
+        Node *tail;
+        
     public:
-        Node *head = nullptr;
-        //Destructor to detroy the linked list and free memory allocated the list.
+
+        LinkedList() {
+            head = nullptr;
+            tail = nullptr;
+        }
+        // Destructor to detroy the linked list and free memory allocated the list.
         // Default destructor only deletes head (allocated by constructor).
         // We need destructor to free the memory used by all individual nodes.
         ~LinkedList() {
@@ -35,14 +42,14 @@ class LinkedList {
                 delete temp;
                 temp = head;
             }
-            cout << endl << "List has been Destroyed...!!!";
+            std::cout << "\nList has been Destroyed...!!!";
         }
         void DeleteAtHead();
         void Insert(int);
         void Display();
 };
 
-/*NOTE:- ->HERE DELETION MEANS POINTING TO THE NEXT->NEXT NODE AND NOT ACTUALLY DELETING THE NODE. 
+/*NOTE:-
          ->Reasons for not using free() :
                 a.Using free() to deallocate memory allocated for an object in C++ is not recommended because it does not call the object's destructor.
                 b.When executed, a "SEGMENTATION FAULT" will occur.
@@ -50,77 +57,68 @@ class LinkedList {
                 d.delete is used when the memory is allocated using "new";
 */
 void LinkedList::DeleteAtHead() {
-    //Check if teh list is empty.
+    // Check if teh list is empty.
     if (head == nullptr) {
-        cout << endl << "The list is empty";
+        std::cout << "\nThe list is empty";
         return;
     }
-    //Deleting head means moving the head to the next node.
+    // Deleting head means moving the head to the next node.
     Node *temp = head;
     head = head->next;
     delete temp;
-     /*NOTE:- If segmentation fault occurs it means you are trying to delete the node that is not available.
-             In other words, given position > total number of nodes.
-    */
 }
 
 void LinkedList::Insert(int data) {
-    //If the list is empty create a Node
+    // If the list is empty create a Node
     Node *newNode = new Node(data);
     if (head == nullptr) {
         head = newNode;
+        tail = newNode;
         return;
     }
-    else {
-        Node *temp = head;
-        //Traverse to the end of the list
-        while(temp->next != nullptr) {
-            temp = temp->next;
-        }
-        temp->next = newNode;
-        return;
-    }
+    tail->next = newNode;
+    tail = tail->next;
 }
 
 void LinkedList::Display() {
-    //Check if the list is empty.
+    // Check if the list is empty.
     if (head == nullptr) {
-        cout << "List is empty" << endl;
+        std::cout << "\nList is empty";
         return;
     }
-    //If the list is not empty
+    // If the list is not empty
     Node *temp = head;
     while (temp != nullptr) {
-        cout << temp->data << endl;
+        std::cout << temp->data << " ";
         temp = temp->next;
     }
 }
 
 int main() {
 
-    //Object of class LinkedList.
-    LinkedList list;
+    // Object of class LinkedList.
+    LinkedList l;
     int temp,ch;
-    //Menu driven program.
+    // Menu driven program.
     do {
-        cout << "\t\t      MENU" << endl;
-        cout << "\t\t1.Insert" << endl;
-        cout << "\t\t2.Delete at HEAD" << endl;
-        cout << "\t\t3.Display" << endl;
-        cout << "\t\t4.Exit" << endl;
-        cout << "Enter your choice: [  ]\b\b";
-        cin >> ch;
+        std::cout << "\n\t\t      MENU";
+        std::cout << "\n\t\t1.Insert";
+        std::cout << "\n\t\t2.Delete at HEAD";
+        std::cout << "\n\t\t3.Display";
+        std::cout << "\n\t\t4.Exit";
+        std::cout << "\nEnter your choice: [  ]\b\b";
+        std::cin >> ch;
         switch(ch) {
                 case 1:
-                    cout << endl << "Enter the data to insert: ";
-                    cin >> temp;
-                    list.Insert(temp);
+                    std::cout << "\nEnter the data to insert: ";
+                    std::cin >> temp;
+                    l.Insert(temp);
                 break;
                 case 2:
-                    list.DeleteAtHead();
+                    l.DeleteAtHead();
                 break;
                 case 3:
-                    list.Display();
+                    l.Display();
                 break;
                 case 4:
                     return 0;
