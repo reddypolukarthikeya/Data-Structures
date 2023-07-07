723683aae7b053e1d35e91b41c2bcea82c35c0de
reddypolukarthikeya <reddypolukarthikeya@gmail.com>
2023-07-07 22:27:35 +0530

    Final commit

--- Linked List/Doubly_Linked_List_Insert.cpp
+++ Linked List/Doubly_Linked_List_Insert.cpp
@@ -1,10 +1,6 @@
 #include <iostream>
 
-
 // Using 'nampespace std;' is considered as bad programming practice.
-using std::cin;
-using std::cout;
-using std::endl;
 
 class Node {
     public:
@@ -27,21 +23,28 @@ class List {
     private:
         Node *head;
     public:
+        // Destructor to detroy the linked list and free memory allocated the list.
         // Default destructor only deletes head (allocated by constructor).
         // We need destructor to free the memory used by all individual nodes.
         List() {
             head = nullptr;
         }
+/*  NOTE:- ->HERE DELETION MEANS POINTING TO THE NEXT->NEXT NODE AND NOT ACTUALLY DELETING THE NODE. 
+            ->Reasons for not using free() :
+                a.Using free() to deallocate memory allocated for an object in C++ is not recommended because it does not call the object's destructor.
+                b.When executed, a "SEGMENTATION FAULT" will occur.
+                c.free() can only be used to deallocate memory that has been allocated using "malloc()" and "calloc()".
+                d.delete is used when the memory is allocated using "new";
+*/
         ~List() {
             Node *temp = head;
             while(head != nullptr) {
                 head = head->next;
-                free (temp);
+                delete temp;
                 temp = head;
             }
-            free(head);
-            free(temp);
-            cout << endl << "The list is destroyed...!!!";
+            delete head;
+            std::cout << "\nThe list is destroyed...!!!";
         }
         void Display();
         void Insert(int);
@@ -65,16 +68,16 @@ void List::Insert(int data) {
 void List::Display() {
     //Check if the list is empty or not.
     if (head == nullptr) {
-        cout << "The list is empty";
+        std::cout << "\nThe list is empty";
         return;
     }
     Node *temp = head;
     while(temp != nullptr) {
         if(temp->next == nullptr) {
-            cout << temp->data;
+            std::cout << temp->data;
             return;
         }
-        cout << temp->data << " <=> ";
+        std::cout << temp->data << " <=> ";
         temp = temp->next;
     }
 }
@@ -83,16 +86,16 @@ int main() {
     List list;
     int ch,temp,p;
     do {    
-        cout << "\t\t         MENU" << endl;
-        cout << "1.Insert" << endl;
-        cout << "2.Display" << endl;
-        cout << "3.Exit" << endl;
-        cout << "Enter your choice: [  ]\b\b";
-        cin >> ch;
+        std::cout << "\n\t\t         MENU";
+        std::cout << "\n1.Insert";
+        std::cout << "\n2.Display";
+        std::cout << "\n3.Exit";
+        std::cout << "\nEnter your choice: [  ]\b\b";
+        std::cin >> ch;
         switch (ch) {
             case 1:
-                cout << endl << "Enter the data to insert:";
-                cin >> temp;
+                std::cout << "\nEnter the data to insert:";
+                std::cin >> temp;
                 list.Insert(temp);
             break;
             case 2:
