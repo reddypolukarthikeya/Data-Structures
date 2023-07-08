c133919b2e11f9b0e022a033daffd84e93bcbafb
reddypolukarthikeya <reddypolukarthikeya@gmail.com>
2023-07-08 09:22:36 +0530

    Final commit

--- Linked List/Singly_Linked_List_Reverse.cc
+++ Linked List/Singly_Linked_List_Reverse.cc
@@ -1,30 +1,43 @@
 #include <iostream>
 
-//Usage of namespace std is considered as bad programming practice.
+// Usiing of "namespace std;" is considered as bad programming practice.
 
 class Node {
     public:
         int data;
         Node *next;
-    
+        // Parameterized constructor.
         Node (int data) {
             this->data = data;
             this->next = nullptr;
         }
 };
 
-class List{
+class LinkedList{
+
     private:
 
         Node *head;
+        Node *tail;
 
     public:
 
-        List() : head(nullptr) {}
-        //Destructor to detroy the linked list and free memory allocated the list.
+        LinkedList() {
+            head = nullptr;
+            tail = nullptr;
+        }
+        // Destructor to detroy the linked list and free memory allocated the list.
         // Default destructor only deletes head (allocated by constructor).
         // We need destructor to free the memory used by all individual nodes.
-        ~List () {
+/*NOTE:-
+    ->Reasons for not using free() :
+        a.Using free() to deallocate memory allocated for an object in C++ is not recommended because 
+            it does not call the object's destructor.
+        b.When executed, a "SEGMENTATION FAULT" will occur.
+        c.free() can only be used to deallocate memory that has been allocated using "malloc()" and "calloc()".
+        d.delete is used when the memory is allocated using "new";
+*/
+        ~LinkedList () {
             Node *temp = head;
             while (head != nullptr) {
                 head = head->next;
@@ -37,31 +50,25 @@ class List{
         void Display();
 };
 
-void List::Insert(int data) {
-    //If the list is empty create a Node
+void LinkedList::Insert(int data) {
     Node *newNode = new Node(data);
+    // Check if the list is emtpy.
     if (head == nullptr) {
         head = newNode;
+        tail = newNode;
         return;
     }
-    else {
-        Node *temp = head;
-        //Traverse to the end of the list
-        while(temp->next != nullptr) {
-            temp = temp->next;
-        }
-        temp->next = newNode;
-        return;
-    }
+    tail->next = newNode;
+    tail = tail->next;
 }
 
-void List::Display() {
-    //Check if the list is empty.
+void LinkedList::Display() {
+    // Check if the list is empty.
     if (head == nullptr) {
         std::cout << "List is empty\n";
         return;
     }
-    //If the list is not empty
+    // If the list is not empty
     Node *temp = head;
     while (temp != nullptr) {
         std::cout << temp->data << " ";
@@ -69,7 +76,7 @@ void List::Display() {
     }
 }
 
-void List::Reverse() {
+void LinkedList::Reverse() {
     // If the list is empty
     if (head == nullptr) {
         std::cout << "The list is empty\n";
@@ -93,8 +100,9 @@ void List::Reverse() {
 }
 
 int main() {
-    List l;
-    int ch;
+    LinkedList l;
+    int ch, temp;
+    // Menu driven program.
     do {
         std::cout << "\tMenu\n";
         std::cout << "1.Insert\n";
@@ -105,7 +113,6 @@ int main() {
         std::cin >> ch;
         switch (ch) {
             case 1:
-                int temp;
                 std::cin >> temp;
                 l.Insert(temp);
             break;
