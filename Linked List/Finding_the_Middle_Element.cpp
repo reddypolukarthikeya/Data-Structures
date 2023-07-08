b93fa2cc195d75f4bcb497cd167a218d7242e305
reddypolukarthikeya <reddypolukarthikeya@gmail.com>
2023-07-08 14:10:43 +0530

    Final commit

--- Linked List/Finding_the_Middle_Element.cpp
+++ Linked List/Finding_the_Middle_Element.cpp
@@ -1,31 +1,53 @@
 #include <iostream>
 
-using std::endl;
-using std::cout;
-using std::cin;
+// Using "namespace std;" is considered as bad programming practice.
 
 class Node {
     public:
         int data;
         Node *next;
+        // Parameterized constructor.
         Node(int data) {
             this->data = data;
             this->next = nullptr;
         }
 };
 
-class List {
+class LinkedList {
+
     private:
+
         Node *head;
+        Node *tail;
+
     public:
-        List() {
+
+        // Unparameterized constructor.
+        LinkedList() {
             head = nullptr;
+            tail = nullptr;
+        }
+        // Destructor to detroy the linked list and free memory allocated the list.
+        // Default destructor only deletes head (allocated by constructor).
+        // We need destructor to free the memory used by all individual nodes.
+/*NOTE:-
+    ->Reasons for not using free() :
+        a.Using free() to deallocate memory allocated for an object in C++ is not recommended because 
+            it does not call the object's destructor.
+        b.When executed, a "SEGMENTATION FAULT" will occur.
+        c.free() can only be used to deallocate memory that has been allocated using "malloc()" and "calloc()".
+        d.delete is used when the memory is allocated using "new";
+*/
+        ~LinkedList() {
+            Node *temp = head;
+            delete temp;
+            head = head->next;
         }
         void Find_Middle();
         void Insert(int data);
 };
 
-void List::Find_Middle() {
+void LinkedList::Find_Middle() {
     // The first pointer moves by one node at a time.
     Node *ptr1 = head;
     // The second poitner moves by two nodes at a time.
@@ -35,46 +57,42 @@ void List::Find_Middle() {
         ptr1 = ptr1->next;
         ptr2 = ptr2->next;
     }
-    cout << ptr1->data << endl;
+    std::cout << ptr1->data << "\n";
 }
 
-void List::Insert(int data) {
-    //If the list is empty create a Node
+void LinkedList::Insert(int data) {
     Node *newNode = new Node(data);
+    // Check if the list is empty.
     if (head == nullptr) {
         head = newNode;
+        tail = newNode;
         return;
     }
-    else {
-        Node *temp = head;
-        //Traverse to the end of the list
-        while(temp->next != nullptr) {
-            temp = temp->next;
-        }
-        temp->next = newNode;
-        return;
-    }
+    tail->next = newNode;
+    tail = tail->next;
 }
 
 int main() {
-    List l;
+    // Object of class LinkedList.
+    LinkedList l;
     int ch;
+    // Menu driven program.
     do {
-        cout << "\tMENU " << endl;
-        cout << "1.Insert" << endl;
-        cout << "2.Find the Middle Element" << endl;
-        cout << "3.Exit" << endl;
-        cout << "Enter your choice : [ ]\b\b";
-        cin >> ch;
+        std::cout << "\n\tMENU " ;
+        std::cout << "\n1.Insert" ;
+        std::cout << "\n2.Find the Middle Element" ;
+        std::cout << "\n3.Exit" ;
+        std::cout << "\nEnter your choice : [ ]\b\b";
+        std::cin >> ch;
         switch (ch) {
             case 1: 
-                cout << "Enter the element to insert : ";
+                std::cout << "\nEnter the element to insert : ";
                 int temp;
-                cin >> temp;
+                std::cin >> temp;
                 l.Insert(temp);
             break;
             case 2: 
-                cout << endl;
+                std::cout << "\n" ;
                 l.Find_Middle();
             break;
             case 3: 
